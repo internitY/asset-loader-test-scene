151470c9145c8480257ef478e44ee2fe290b212e
Eric <eric.kirschstein@gmail.com>
2020-09-13 22:20:34 +0200

    some scene adjustments, helper to avoid infinity falling

---
+++ Assets/Scripts/ResetPositionTo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ResetPositionTo : MonoBehaviour
+{
+    public Transform targetPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(targetPos != null)
+            other.transform.position = targetPos.position;
+    }
+}
